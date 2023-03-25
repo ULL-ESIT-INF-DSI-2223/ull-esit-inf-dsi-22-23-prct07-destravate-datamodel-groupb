478b23fe0eef709936acee7cd34862620e646a63
alu0101410869 <alu0101410869@ull.edu.es>
2023-03-25 12:24:34 +0000

    Estadisticas entrenamiento

--- src/estadisticas_entrenamiento.ts
+++ src/estadisticas_entrenamiento.ts
@@ -1,5 +1,35 @@
 export class EstadisticasEntrenamiento {
-    private estadisticaSemanal: [number, number];
-    private estadisticaMensual: [number, number];
-    private estadisticaAnual: [number, number];
+    private _estadisticaSemanal: [number, number];
+    private _estadisticaMensual: [number, number];
+    private _estadisticaAnual: [number, number];
+
+    constructor(estadistcaSemanal: [number, number], estadisticaMensual: [number, number], estadisticaAnual: [number, number]) {
+        this._estadisticaSemanal = estadistcaSemanal;
+        this._estadisticaMensual = estadisticaMensual;
+        this._estadisticaAnual = estadisticaAnual;
+    }
+
+    get estadisticaSemanal(): [number, number] {
+        return this._estadisticaSemanal;
+    }
+
+    set estadisticaSemanal(nuevaSemanal: [number, number]) {
+        this._estadisticaSemanal = nuevaSemanal;
+    }
+
+    get estadisticaMensual(): [number, number] {
+        return this._estadisticaMensual;
+    }
+
+    set estadisticaMensual(nuevaMensual: [number, number]) {
+        this._estadisticaMensual = nuevaMensual;
+    }
+
+    get estadisticaAnual(): [number, number] {
+        return this._estadisticaAnual;
+    }
+
+    set estadisticaAnual(nuevaAnual: [number, number]) {
+        this._estadisticaAnual = nuevaAnual;
+    }
 }
